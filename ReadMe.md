75744cddaac8991f850cff8b2d9d947b233cab6e
Pankaj Sahu <sahupankaj10@gmail.com>
2018-11-19 19:44:39 +0900

    Initiala commit

---
+++ ReadMe.md
@@ -0,0 +1,21 @@
+# Text Summarization using TextRank
+
+### Feature:
+- Based on **Extractive Summarization**:
+ which rely on extracting several parts, such as phrases and sentences, from a piece of text and stack them together to create a summary.
+- **Single domain multiple source summary**
+
+
+## Install:
+```python
+pip install -r requirement.txt
+```
+
+## Uses:
+```python
+python text_summarizer.py
+```
+
+## Data file:
+- **tennis_articles_v4.csv**: ten tennis news article about single event from multiple source
+- **glove.6B.100d.txt**: [*GloVe*](https://nlp.stanford.edu/projects/glove/) : Word embeddings for vector representation of words. Using the pre-trained *Wikipedia 2014 + Gigaword 5* GloVe vectors
---
+++ text_summarizer.py
@@ -0,0 +1,81 @@
+# -- code :UTF-8 --
+
+import numpy as np
+import pandas as pd
+import nltk
+import networkx
+from nltk.tokenize import sent_tokenize
+from sklearn.metrics.pairwise import cosine_similarity
+from nltk.corpus import stopwords
+
+nltk.download('punkt') # one time execution
+nltk.download('stopwords')
+
+# single domain multiple documentation article
+df = pd.read_csv('tennis_articles_v4.csv')
+
+sentences = []
+for s in df['article_text']:
+    sentences.append(sent_tokenize(s))
+
+sentences = [y for x in sentences for y in x]
+
+# Extract word vectors
+# GloVe- word embeddings are vector representation of words.
+# using GloVe also for maintaining the order
+word_embeddings = {}
+f = open('glove.6B.100d.txt', encoding="utf-8")
+for line in f:
+    values = line.split()
+    word = values[0]
+    coefs = np.asarray(values[1:], dtype="float32")
+    word_embeddings[word] = coefs
+f.close()
+
+# remove punctuations, numbers and special characters
+clean_sentences = pd.Series(sentences).str.replace("[^a-zA-Z]", " ")
+
+# make alphabets lowercase
+clean_sentences = [s.lower() for s in clean_sentences]
+
+
+# function to remove stopwords
+def remove_stop_words(sen, lang='English'):
+    stop_words = stopwords.words(lang)
+    sentence_new = " ".join([i for i in sen if i not in stop_words])
+    return sentence_new
+
+
+# remove stopwords
+clean_sentences = [remove_stop_words(r.split()) for r in clean_sentences]
+
+# create a word-vector each with size 100 of each sentence
+sentence_vectors = []
+for sen in clean_sentences:
+    if len(sen) != 0:
+        v = sum([word_embeddings.get(w, np.zeros((100,))) for w in sen.split()])/(len(sen.split())+0.001)
+    else:
+        v = np.zeros((100,))
+    sentence_vectors.append(v)
+
+# similarity matrix
+sim_mat = np.zeros([len(sentences), len(sentences)])
+
+# cosine similarity to check similarity between sentences
+for i in range(len(sentences)):
+    for j in range(len(sentences)):
+        if i != j:
+            sim_mat[i][j] = cosine_similarity(sentence_vectors[i].reshape(1, 100),
+                                              sentence_vectors[j].reshape(1, 100))[0, 0]
+
+# making a graph by applying pageRank algo
+nx_graph = networkx.from_numpy_array(sim_mat)
+scores = networkx.pagerank(nx_graph)
+ranked_scores = sorted(((scores[i], s) for i,s in enumerate(sentences)), reverse=True)
+
+# Extract top 10 sentences as the summary
+for i in range(10):
+    print(ranked_scores[i][1])
+
+
+
